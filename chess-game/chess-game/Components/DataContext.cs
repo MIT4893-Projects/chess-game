1a4dc8165d1bcf3a6550b5dbb92e3b99232f932c
MIT4893-Projects <minhngoc2891211@gmail.com>
2023-01-17 21:02:45 +0700

    Add comments and regions

--- chess-game/chess-game/Components/DataContext.cs
+++ chess-game/chess-game/Components/DataContext.cs
@@ -8,12 +8,24 @@ using System.Threading.Tasks;
 
 namespace chess_game.Components
 {
+    /// <summary>
+    /// Base DataContext class with PropertyChanged event.
+    /// </summary>
     internal class DataContext : INotifyPropertyChanged
     {
+        #region Event handlers
+
         public event PropertyChangedEventHandler PropertyChanged;
+
+        /// <summary>
+        /// Notify a property has changed.
+        /// </summary>
+        /// <param name="propertyName">Name of the changed property.</param>
         public void OnPropertyChange(string propertyName)
         {
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
+
+        #endregion
     }
 }
