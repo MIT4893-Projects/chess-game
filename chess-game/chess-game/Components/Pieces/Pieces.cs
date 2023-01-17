5717609924605bf2c2da74ba929930a2b5492666
MIT4893-Projects <minhngoc2891211@gmail.com>
2023-01-17 19:32:57 +0700

    Add PieceImageIcon static class to display icon for Pieces on chessboard.

--- chess-game/chess-game/Components/Pieces/Pieces.cs
+++ chess-game/chess-game/Components/Pieces/Pieces.cs
@@ -1,7 +1,9 @@
 ﻿using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Media.Imaging;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -35,5 +37,70 @@ namespace chess_game.Components.Pieces
             SetRowPosition(row);
             SetColumnPosition(col);
         }
+    }
+
+    static class PieceImageIcon
+    {
+        private static string CurrentDirectory = Directory.GetCurrentDirectory();
+
+        public static Image BlackBishop = new()
+        {
+            Source = new BitmapImage(new Uri(new Uri(CurrentDirectory, UriKind.Absolute), new Uri(@"/Images/BlackBishop.png", UriKind.Relative)))
+        };
+
+        public static Image BlackKing = new()
+        {
+            Source = new BitmapImage(new Uri(new Uri(CurrentDirectory, UriKind.Absolute), new Uri(@"/Images/BlackKing.png", UriKind.Relative)))
+        };
+
+        public static Image BlackKnight = new()
+        {
+            Source = new BitmapImage(new Uri(new Uri(CurrentDirectory, UriKind.Absolute), new Uri(@"/Images/BlackKnight.png", UriKind.Relative)))
+        };
+
+        public static Image BlackPawn = new()
+        {
+            Source = new BitmapImage(new Uri(new Uri(CurrentDirectory, UriKind.Absolute), new Uri(@"/Images/BlackPawn.png", UriKind.Relative)))
+        };
+
+        public static Image BlackQueen = new()
+        {
+            Source = new BitmapImage(new Uri(new Uri(CurrentDirectory, UriKind.Absolute), new Uri(@"/Images/BlackKing.png", UriKind.Relative)))
+        };
+
+        public static Image BlackRook = new()
+        {
+            Source = new BitmapImage(new Uri(new Uri(CurrentDirectory, UriKind.Absolute), new Uri(@"/Images/BlackRook.png", UriKind.Relative)))
+        };
+
+        public static Image WhiteBishop = new()
+        {
+            Source = new BitmapImage(new Uri(new Uri(CurrentDirectory, UriKind.Absolute), new Uri(@"/Images/WhiteBishop.png", UriKind.Relative)))
+        };
+
+        public static Image WhiteKing = new()
+        {
+            Source = new BitmapImage(new Uri(new Uri(CurrentDirectory, UriKind.Absolute), new Uri(@"/Images/WhiteKing.png", UriKind.Relative)))
+        };
+
+        public static Image WhiteKnight = new()
+        {
+            Source = new BitmapImage(new Uri(new Uri(CurrentDirectory, UriKind.Absolute), new Uri(@"/Images/WhiteKnight.png", UriKind.Relative)))
+        };
+
+        public static Image WhitePawn = new()
+        {
+            Source = new BitmapImage(new Uri(new Uri(CurrentDirectory, UriKind.Absolute), new Uri(@"/Images/WhitePawn.png", UriKind.Relative)))
+        };
+
+        public static Image WhiteQueen = new()
+        {
+            Source = new BitmapImage(new Uri(new Uri(CurrentDirectory, UriKind.Absolute), new Uri(@"/Images/WhiteKing.png", UriKind.Relative)))
+        };
+
+        public static Image WhiteRook = new()
+        {
+            Source = new BitmapImage(new Uri(new Uri(CurrentDirectory, UriKind.Absolute), new Uri(@"/Images/WhiteRook.png", UriKind.Relative)))
+        };
     }
 }
