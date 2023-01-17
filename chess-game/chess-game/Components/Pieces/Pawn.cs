83bda741dc88390006e8e21f769bb353f7e7c046
MIT4893-Projects <minhngoc2891211@gmail.com>
2023-01-17 16:19:35 +0700

    Define classes for pieces

---
+++ chess-game/chess-game/Components/Pieces/Pawn.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Media.Imaging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace chess_game.Components.Pieces
+{
+    internal class Pawn : Piece
+    {
+        public Pawn()
+        {
+        }
+    }
+}
---
+++ chess-game/chess-game/Components/Pieces/Pieces.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace chess_game.Components.Pieces
+{
+    internal class Piece : Button
+    {
+        public int RowPosition { get; private set; } = 0;
+        public int ColumnPosition { get; private set; } = 0;
+
+        public Piece()
+        {
+            //Style = Application.Current.Resources["PieceButton"] as Style;
+        }
+
+        public void SetRowPosition(int value)
+        {
+            if (0 <= value && value < 8)
+                RowPosition = value;
+        }
+
+        public void SetColumnPosition(int value)
+        {
+            if (0 <= value && value < 8)
+                ColumnPosition = value;
+        }
+
+        public void SetRowColumnPosition(int row, int col)
+        {
+            SetRowPosition(row);
+            SetColumnPosition(col);
+        }
+    }
+}
