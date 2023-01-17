7a9022ed4563cff41a8d434a7ae70b6fb44e7741
MIT4893-Projects <minhngoc2891211@gmail.com>
2023-01-17 16:23:15 +0700

    Define DataContext base class inherit for INotifyPropertyChanged.

---
+++ chess-game/chess-game/Components/DataContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.UI.Xaml;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace chess_game.Components
+{
+    internal class DataContext : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChange(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
