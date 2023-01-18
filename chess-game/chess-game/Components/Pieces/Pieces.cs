a7c607f36cfa1becf51545ab808eba06dfc6d377
MIT4893-Projects <minhngoc2891211@gmail.com>
2023-01-18 23:53:56 +0700

    Code Cleanup (remove unused using).

--- chess-game/chess-game/Components/Pieces/Pieces.cs
+++ chess-game/chess-game/Components/Pieces/Pieces.cs
@@ -1,14 +1,7 @@
-﻿using Microsoft.UI;
-using Microsoft.UI.Xaml;
+﻿using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
 using Microsoft.UI.Xaml.Controls.Primitives;
-using Microsoft.UI.Xaml.Media;
 using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace chess_game.Components.Pieces
 {
