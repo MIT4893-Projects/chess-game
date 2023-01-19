5962cef65abf692a48a6a9c69deebe61f7fa45e1
MIT4893-Projects <minhngoc2891211@gmail.com>
2023-01-19 17:02:03 +0700

    Create the MoveableCell class to describe a Moveable Cell. Add inheritance from IEnumerable<Piece> interface to be able to add objects of this class to a list. Add ToString method.

---
+++ chess-game/chess-game/Components/Pieces/MoveableCell.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace chess_game.Components.Pieces
+{
+    internal class MoveableCell : IEnumerable<Piece>
+    {
+        public int Row;
+        public int Column;
+
+        public IEnumerator<Piece> GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string ToString()
+        {
+            return $"{Row} {Column}";
+        }
+    }
+}
